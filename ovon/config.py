05e584ef486b0bf631123c57c6044514263af912
AnandSingh-0619 <anandsingh16898@gmail.com>
2024-10-29 22:12:50 -0400

    train rnn ddppo

--- ovon/config.py
+++ ovon/config.py
@@ -331,3 +331,7 @@ class HabitatConfigPlugin(SearchPathPlugin):
             provider="habitat_baselines",
             path="pkg://habitat_baselines/config/",
         )
+        search_path.append(
+            provider="ovon",
+            path="/nethome/asingh3064/flash/ovon/config/tasks",
+        )
