fe13a185ab7e0ff929a327027e9218e316055ef3
AnandSingh-0619 <anandsingh16898@gmail.com>
2024-11-01 16:23:31 -0400

    train

--- my_scripts/eval/eval_rnn_ddppo.sh
+++ my_scripts/eval/eval_rnn_ddppo.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=ovon-train
-#SBATCH --output=slurm_logs/train/ovon-ddppo-%j.out
-#SBATCH --error=slurm_logs/train/ovon-ddppo-%j.err
-#SBATCH --gpus a40:4
+#SBATCH --job-name=ovon-eval
+#SBATCH --output=slurm_logs/eval/ovon-ddppo-%j.out
+#SBATCH --error=slurm_logs/eval/ovon-ddppo-%j.err
+#SBATCH --gpus a40:1
 #SBATCH --nodes 1
 #SBATCH --cpus-per-task 10
-#SBATCH --ntasks-per-node 4
+#SBATCH --ntasks-per-node 1
 #SBATCH --partition=kira-lab,overcap
 #SBATCH --qos=short
 #SBATCH --signal=USR1@100
-#SBATCH --requeue
+
 
 export GLOG_minloglevel=2
 export HABITAT_SIM_LOG=quiet
@@ -23,19 +23,19 @@ source /nethome/asingh3064/flash/envs/etc/profile.d/conda.sh
 conda deactivate
 conda activate ovon
 
-TENSORBOARD_DIR="tb/objectnav/train/ddppo_${JOB_ID}"
-CHECKPOINT_DIR="data/new_checkpoints/objectnav/ddppo_997723"
+TENSORBOARD_DIR="tb/objectnav/eval/ddppo_${JOB_ID}"
+CHECKPOINT_DIR="data/checkpoints/eval/"
 LOG_DIR="Logs/ddppo_rnn_${JOB_ID}.log"
-split="train"
+split="val_seen_synonyms"
 
 srun python -um ovon.run \
-  --run-type train \
+  --run-type eval \
   --exp-config config/experiments/rnn_rl.yaml \
   habitat_baselines.trainer_name="ddppo_no_2d" \
   habitat_baselines.tensorboard_dir=${TENSORBOARD_DIR} \
   habitat_baselines.checkpoint_folder=${CHECKPOINT_DIR} \
+  habitat_baselines.eval_ckpt_path_dir=${CHECKPOINT_DIR} \
   habitat_baselines.log_file=${LOG_DIR} \
   habitat_baselines.num_environments=24 \
-  habitat.dataset.data_path=data/datasets/ovon/hm3d/$split/$split.json.gz \
-  habitat_baselines.load_resume_state_config=True \
-  habitat_baselines.writer_type="wb"
+  habitat.dataset.data_path=data/datasets/ovon/hm3d/val_seen_synonyms/val_unseen_easy.json.gz \
+  habitat_baselines.load_resume_state_config=False 
