b05f8efb7ead5c5c98435018a2bbc7b6a05edc07
AnandSingh-0619 <anandsingh16898@gmail.com>
2024-11-08 14:05:33 -0500

    eval trainer changes

--- my_scripts/eval/eval_od_ddppo.sh
+++ my_scripts/eval/eval_od_ddppo.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=ovon-train
-#SBATCH --output=slurm_logs/train/ovon-ddppo-%j.out
-#SBATCH --error=slurm_logs/train/ovon-ddppo-%j.err
-#SBATCH --gpus a40:4
+#SBATCH --job-name=ovon-eval
+#SBATCH --output=slurm_logs/eval/ovon-ddppo-%j.out
+#SBATCH --error=slurm_logs/eval/ovon-ddppo-%j.err
+#SBATCH --gpus a40:1
 #SBATCH --nodes 1
 #SBATCH --cpus-per-task 10
-#SBATCH --ntasks-per-node 4
+#SBATCH --ntasks-per-node 1
 #SBATCH --partition=kira-lab,overcap
 #SBATCH --qos=short
+#SBATCH --exclude=omgwth,clippy,major,baymax,ig-88
 #SBATCH --signal=USR1@100
-#SBATCH --exclude=omgwth,clippy,major
 #SBATCH --requeue
 
 export GLOG_minloglevel=2
@@ -24,20 +24,22 @@ source /nethome/asingh3064/flash/envs/etc/profile.d/conda.sh
 conda deactivate
 conda activate ovonv3
 
-TENSORBOARD_DIR="tb/objectnav/train/od_ddppo_${JOB_ID}"
-CHECKPOINT_DIR="data/new_checkpoints/objectnav/od_ddppo_999360"
-LOG_DIR="Logs/ddppo_od_rnn_${JOB_ID}.log"
-split="train"
+split="val_unseen"
+
+TENSORBOARD_DIR="tb/objectnav/eval/ddppo_${JOB_ID}"
+CHECKPOINT_DIR="data/checkpoints/eval_od/"
+LOG_DIR="Logs/eval_ddppo_od_${JOB_ID}.log"
 
 srun python -um ovon.run \
-  --run-type train \
+  --run-type eval \
   --exp-config config/experiments/rnn_rl.yaml \
   habitat_baselines.trainer_name="ddppo_od" \
   habitat_baselines.tensorboard_dir=${TENSORBOARD_DIR} \
   habitat_baselines.checkpoint_folder=${CHECKPOINT_DIR} \
+  habitat_baselines.eval_ckpt_path_dir=${CHECKPOINT_DIR} \
   habitat_baselines.log_file=${LOG_DIR} \
   habitat_baselines.rl.policy.name=PointNavResNetODPolicy \
   habitat_baselines.num_environments=24 \
-  habitat.dataset.data_path=data/datasets/ovon/hm3d/$split/$split.json.gz \
-  habitat_baselines.load_resume_state_config=True \
-  habitat_baselines.writer_type="wb"
+  habitat.dataset.data_path=data/datasets/ovon/hm3d/val_seen/val_seen.json.gz \
+  habitat_baselines.load_resume_state_config=False 
+
--- ovon/trainers/ppo_od_trainer.py
+++ ovon/trainers/ppo_od_trainer.py
@@ -18,6 +18,22 @@ from habitat_baselines.utils.common import (
     is_continuous_action_space,
 )
 import time
+from habitat_baselines.common.tensorboard_utils import (
+    TensorboardWriter,
+    get_writer,
+)
+from habitat.config import read_write
+from habitat.config.default import get_agent_config
+from habitat import VectorEnv, logger
+from omegaconf import OmegaConf
+from typing import TYPE_CHECKING, Any, Dict, List, Optional
+from collections import defaultdict, deque
+import os
+import tqdm
+from habitat.utils.visualizations.utils import observations_to_image
+from habitat.utils.render_wrapper import overlay_frame
+from habitat.tasks.rearrange.rearrange_sensors import GfxReplayMeasure
+from habitat.tasks.rearrange.utils import write_gfx_replay
 
 @baseline_registry.register_trainer(name="ddppo_od")
 @baseline_registry.register_trainer(name="ppo_od")
@@ -152,3 +168,332 @@ class PPO_ODTrainer(PPOTrainer):
         self.pth_time += time.time() - t_update_stats
 
         return env_slice.stop - env_slice.start
+    
+    
+    def _eval_checkpoint(
+        self,
+        checkpoint_path: str,
+        writer: TensorboardWriter,
+        checkpoint_index: int = 0,
+    ) -> None:
+        r"""Evaluates a single checkpoint.
+
+        Args:
+            checkpoint_path: path of checkpoint
+            writer: tensorboard writer object for logging to tensorboard
+            checkpoint_index: index of cur checkpoint for logging
+
+        Returns:
+            None
+        """
+        if self._is_distributed:
+            raise RuntimeError("Evaluation does not support distributed mode")
+
+        # Map location CPU is almost always better than mapping to a CUDA device.
+        if self.config.habitat_baselines.eval.should_load_ckpt:
+            ckpt_dict = self.load_checkpoint(
+                checkpoint_path, map_location="cpu"
+            )
+            step_id = ckpt_dict["extra_state"]["step"]
+            print(step_id)
+        else:
+            ckpt_dict = {"config": None}
+
+        config = self._get_resume_state_config_or_new_config(
+            ckpt_dict["config"]
+        )
+
+        ppo_cfg = config.habitat_baselines.rl.ppo
+
+        with read_write(config):
+            config.habitat.dataset.split = config.habitat_baselines.eval.split
+            config.habitat.dataset.split='val_unseen'
+            config.habitat.dataset.data_path='data/datasets/ovon/hm3d/val_unseen/val_unseen_hard.json.gz'
+            
+
+        if (
+            len(config.habitat_baselines.video_render_views) > 0
+            and len(self.config.habitat_baselines.eval.video_option) > 0
+        ):
+            agent_config = get_agent_config(config.habitat.simulator)
+            agent_sensors = agent_config.sim_sensors
+            render_view_uuids = [
+                agent_sensors[render_view].uuid
+                for render_view in config.habitat_baselines.video_render_views
+                if render_view in agent_sensors
+            ]
+            assert len(render_view_uuids) > 0, (
+                f"Missing render sensors in agent config: "
+                f"{config.habitat_baselines.video_render_views}."
+            )
+            with read_write(config):
+                for render_view_uuid in render_view_uuids:
+                    if render_view_uuid not in config.habitat.gym.obs_keys:
+                        config.habitat.gym.obs_keys.append(render_view_uuid)
+                config.habitat.simulator.debug_render = True
+
+        if config.habitat_baselines.verbose:
+            logger.info(f"env config: {OmegaConf.to_yaml(config)}")
+
+        self._init_envs(config, is_eval=True)
+
+        action_space = self.envs.action_spaces[0]
+        self.policy_action_space = action_space
+        self.orig_policy_action_space = self.envs.orig_action_spaces[0]
+        if is_continuous_action_space(action_space):
+            # Assume NONE of the actions are discrete
+            action_shape = (get_num_actions(action_space),)
+            discrete_actions = False
+        else:
+            # For discrete pointnav
+            action_shape = (1,)
+            discrete_actions = True
+
+        self._setup_actor_critic_agent(ppo_cfg)
+
+        if self.agent.actor_critic.should_load_agent_state:
+            self.agent.load_state_dict(ckpt_dict["state_dict"])
+        self.actor_critic = self.agent.actor_critic
+
+        observations = self.envs.reset()
+        batch = batch_obs(observations, device=self.device)
+        batch = apply_obs_transforms_batch(batch, self.obs_transforms)  # type: ignore
+
+        current_episode_reward = torch.zeros(
+            self.envs.num_envs, 1, device="cpu"
+        )
+
+        test_recurrent_hidden_states = torch.zeros(
+            self.config.habitat_baselines.num_environments,
+            self.actor_critic.num_recurrent_layers,
+            ppo_cfg.hidden_size,
+            device=self.device,
+        )
+        prev_actions = torch.zeros(
+            self.config.habitat_baselines.num_environments,
+            *action_shape,
+            device=self.device,
+            dtype=torch.long if discrete_actions else torch.float,
+        )
+        not_done_masks = torch.zeros(
+            self.config.habitat_baselines.num_environments,
+            1,
+            device=self.device,
+            dtype=torch.bool,
+        )
+        stats_episodes: Dict[
+            Any, Any
+        ] = {}  # dict of dicts that stores stats per episode
+        ep_eval_count: Dict[Any, int] = defaultdict(lambda: 0)
+
+        rgb_frames = [
+            [] for _ in range(self.config.habitat_baselines.num_environments)
+        ]  # type: List[List[np.ndarray]]
+        if len(self.config.habitat_baselines.eval.video_option) > 0:
+            os.makedirs(self.config.habitat_baselines.video_dir, exist_ok=True)
+
+        number_of_eval_episodes = (
+            self.config.habitat_baselines.test_episode_count
+        )
+        evals_per_ep = self.config.habitat_baselines.eval.evals_per_ep
+        if number_of_eval_episodes == -1:
+            number_of_eval_episodes = sum(self.envs.number_of_episodes)
+        else:
+            total_num_eps = sum(self.envs.number_of_episodes)
+            # if total_num_eps is negative, it means the number of evaluation episodes is unknown
+            if total_num_eps < number_of_eval_episodes and total_num_eps > 1:
+                logger.warn(
+                    f"Config specified {number_of_eval_episodes} eval episodes"
+                    ", dataset only has {total_num_eps}."
+                )
+                logger.warn(f"Evaluating with {total_num_eps} instead.")
+                number_of_eval_episodes = total_num_eps
+            else:
+                assert evals_per_ep == 1
+        assert (
+            number_of_eval_episodes > 0
+        ), "You must specify a number of evaluation episodes with test_episode_count"
+
+        pbar = tqdm.tqdm(total=number_of_eval_episodes * evals_per_ep)
+        self.actor_critic.eval()
+        while (
+            len(stats_episodes) < (number_of_eval_episodes * evals_per_ep)
+            and self.envs.num_envs > 0
+        ):
+            current_episodes_info = self.envs.current_episodes()
+
+            with inference_mode():
+                (
+                    _,
+                    actions,
+                    _,
+                    test_recurrent_hidden_states,
+                ) = self.actor_critic.act(
+                    batch,
+                    test_recurrent_hidden_states,
+                    prev_actions,
+                    not_done_masks,
+                    deterministic=False,
+                )
+
+                prev_actions.copy_(actions)  # type: ignore
+            # NB: Move actions to CPU.  If CUDA tensors are
+            # sent in to env.step(), that will create CUDA contexts
+            # in the subprocesses.
+            if is_continuous_action_space(self.policy_action_space):
+                # Clipping actions to the specified limits
+                step_data = [
+                    np.clip(
+                        a.numpy(),
+                        self.policy_action_space.low,
+                        self.policy_action_space.high,
+                    )
+                    for a in actions.cpu()
+                ]
+            else:
+                step_data = [a.item() for a in actions.cpu()]
+
+            outputs = self.envs.step(step_data)
+
+            observations, rewards_l, dones, infos = [
+                list(x) for x in zip(*outputs)
+            ]
+            policy_info = self.actor_critic.get_policy_info(infos, dones)
+            for i in range(len(policy_info)):
+                infos[i].update(policy_info[i])
+            batch = batch_obs(  # type: ignore
+                observations,
+                device=self.device,
+            )
+            batch = apply_obs_transforms_batch(batch, self.obs_transforms)  # type: ignore
+
+            not_done_masks = torch.tensor(
+                [[not done] for done in dones],
+                dtype=torch.bool,
+                device="cpu",
+            )
+
+            rewards = torch.tensor(
+                rewards_l, dtype=torch.float, device="cpu"
+            ).unsqueeze(1)
+            current_episode_reward += rewards
+            next_episodes_info = self.envs.current_episodes()
+            envs_to_pause = []
+            n_envs = self.envs.num_envs
+            for i in range(n_envs):
+                if (
+                    ep_eval_count[
+                        (
+                            next_episodes_info[i].scene_id,
+                            next_episodes_info[i].episode_id,
+                        )
+                    ]
+                    == evals_per_ep
+                ):
+                    envs_to_pause.append(i)
+
+                if len(self.config.habitat_baselines.eval.video_option) > 0:
+                    # TODO move normalization / channel changing out of the policy and undo it here
+                    frame = observations_to_image(
+                        {k: v[i] for k, v in batch.items()}, infos[i]
+                    )
+                    if not not_done_masks[i].item():
+                        # The last frame corresponds to the first frame of the next episode
+                        # but the info is correct. So we use a black frame
+                        frame = observations_to_image(
+                            {k: v[i] * 0.0 for k, v in batch.items()}, infos[i]
+                        )
+                    frame = overlay_frame(frame, infos[i])
+                    rgb_frames[i].append(frame)
+
+                # episode ended
+                if not not_done_masks[i].item():
+                    pbar.update()
+                    episode_stats = {
+                        "reward": current_episode_reward[i].item()
+                    }
+                    episode_stats.update(
+                        self._extract_scalars_from_info(infos[i])
+                    )
+                    current_episode_reward[i] = 0
+                    k = (
+                        current_episodes_info[i].scene_id,
+                        current_episodes_info[i].episode_id,
+                    )
+                    ep_eval_count[k] += 1
+                    # use scene_id + episode_id as unique id for storing stats
+                    stats_episodes[(k, ep_eval_count[k])] = episode_stats
+
+                    if (
+                        len(self.config.habitat_baselines.eval.video_option)
+                        > 0
+                    ):
+                        generate_video(
+                            video_option=self.config.habitat_baselines.eval.video_option,
+                            video_dir=self.config.habitat_baselines.video_dir,
+                            images=rgb_frames[i],
+                            episode_id=current_episodes_info[i].episode_id,
+                            checkpoint_idx=checkpoint_index,
+                            metrics=self._extract_scalars_from_info(infos[i]),
+                            fps=self.config.habitat_baselines.video_fps,
+                            tb_writer=writer,
+                            keys_to_include_in_name=self.config.habitat_baselines.eval_keys_to_include_in_name,
+                        )
+
+                        rgb_frames[i] = []
+
+                    gfx_str = infos[i].get(GfxReplayMeasure.cls_uuid, "")
+                    if gfx_str != "":
+                        write_gfx_replay(
+                            gfx_str,
+                            self.config.habitat.task,
+                            current_episodes_info[i].episode_id,
+                        )
+
+            not_done_masks = not_done_masks.to(device=self.device)
+            (
+                self.envs,
+                test_recurrent_hidden_states,
+                not_done_masks,
+                current_episode_reward,
+                prev_actions,
+                batch,
+                rgb_frames,
+            ) = self._pause_envs(
+                envs_to_pause,
+                self.envs,
+                test_recurrent_hidden_states,
+                not_done_masks,
+                current_episode_reward,
+                prev_actions,
+                batch,
+                rgb_frames,
+            )
+
+        pbar.close()
+        assert (
+            len(ep_eval_count) >= number_of_eval_episodes
+        ), f"Expected {number_of_eval_episodes} episodes, got {len(ep_eval_count)}."
+
+        aggregated_stats = {}
+        for stat_key in next(iter(stats_episodes.values())).keys():
+            aggregated_stats[stat_key] = np.mean(
+                [v[stat_key] for v in stats_episodes.values()]
+            )
+
+        for k, v in aggregated_stats.items():
+            logger.info(f"Average episode {k}: {v:.4f}")
+
+        step_id = checkpoint_index
+        if "extra_state" in ckpt_dict and "step" in ckpt_dict["extra_state"]:
+            step_id = ckpt_dict["extra_state"]["step"]
+
+        writer.add_scalar(
+            "eval_reward/average_reward", aggregated_stats["reward"], step_id
+        )
+
+        metrics = {k: v for k, v in aggregated_stats.items() if k != "reward"}
+        for k, v in metrics.items():
+            writer.add_scalar(f"eval_metrics/{k}", v, step_id)
+
+        self.envs.close()
